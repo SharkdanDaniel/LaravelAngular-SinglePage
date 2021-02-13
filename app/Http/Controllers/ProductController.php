72a2cc3426a8faf87de8a8e02cd853af33aa7d20
Daniel Cardozo <sharkdan_daniel@hotmail.com>
2021-02-13 20:03:36 -0300

    Corrigindo o response all

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,7 +16,7 @@ class ProductController extends Controller
     public function index()
     {   
         $products = Product::all();
-        return response(['data' => $products]);
+        return response($products);
     }
 
     public function store(Request $request)
